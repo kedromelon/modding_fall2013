c2552a2546a7e6f56c24b8bbd6075e8777dafbbb
Noah Lemen <noah.lemen@gmail.com>
2013-10-17 11:48:11 -0400

    added week 7 (10print port)

---
+++ modding_week7/Assets/scripts/TenPrint.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TenPrint : MonoBehaviour {
+	
+	TextMesh myTextMesh;
+	int counter;
+	
+	// Use this for initialization
+	void Start () {
+		myTextMesh = GetComponent<TextMesh>();
+		counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float randomNumber = Random.Range ( 0f, 10f );
+		
+		if ( randomNumber < 5f ) {
+			myTextMesh.text += "/";
+		} else {
+			myTextMesh.text += "\\";
+		}
+		
+		counter++;
+		
+		if ( counter % 40 == 0){
+			myTextMesh.text += "\n";
+		}
+	
+	}
+}
