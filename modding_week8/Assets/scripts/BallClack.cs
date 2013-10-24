83852a672bf7aff6ea4594f6f6193cb52a509ea3
Noah Lemen <noah.lemen@gmail.com>
2013-10-24 14:07:49 -0400

    added week 8: ball clack & 3d platformer

---
+++ modding_week8/Assets/scripts/BallClack.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallClack : MonoBehaviour {
+	
+	public Vector3 start, end; // exposed in the inspector
+	
+	Vector3 baseCameraPosition;
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine( BallMove() );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	IEnumerator ScreenShake () {
+		float t = .5f;
+		baseCameraPosition = Camera.main.transform.position;
+		while ( t > 0f ){
+			t -= Time.deltaTime;
+			Camera.main.transform.position = baseCameraPosition + new Vector3( Mathf.Sin(Time.time * 10f)*.3f, 
+																 			   Mathf.Sin(Time.time * 12.5f)*.5f, 
+														 					   Mathf.Sin(Time.time * 7f)*.2f);
+			yield return 0;
+		}
+	}
+	
+	IEnumerator BallMove () {
+		while( true ){
+			float t = Mathf.Sin(Time.time * 2f) * 0.5f + 0.5f;
+			if ((Mathf.Abs( 0.5f - t ) < 0.01f) && !audio.isPlaying){
+				audio.Play();
+				//StartCoroutine(ScreenShake());
+			}
+			transform.position = Vector3.Lerp( start, end, t );
+			yield return 0; // wait a frame
+		}
+		
+		// this code will never run bc while loop is infinite
+		yield return 0;
+		Debug.Log ("I waited one frame");
+		
+		yield return 0;
+		yield return 0;
+		Debug.Log("I waited two frames");
+		
+		yield return new WaitForSeconds( 5f );
+		Debug.Log("5 Seconds have passed");
+	}
+}
---
+++ modding_week8/Assets/scripts/Platformer.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platformer : MonoBehaviour {
+	
+	public float speed = 1;
+	public float turnSpeed = 1;
+	public float jumpSpeed = 1;
+	public float fallSpeed = 1;
+	Vector3 moveVector;
+	bool grounded = false;
+	
+	// Use this for initialization
+	void Start () {
+		moveVector = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		moveVector = Vector3.zero;
+		
+		Ray ray = new Ray(transform.position, -transform.up);
+		RaycastHit hit = new RaycastHit();
+		
+		if (Physics.Raycast(ray, out hit, 1.25f)){
+			grounded = true;
+		}else{
+			grounded = false;
+		}
+		
+		if(Input.GetKey(KeyCode.W)){
+			moveVector += transform.forward * speed;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			moveVector -= transform.forward * speed;
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate( 0f, -turnSpeed * Time.deltaTime, 0f );
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate( 0f, turnSpeed * Time.deltaTime, 0f );
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Space) && grounded == true){
+			moveVector += transform.up * jumpSpeed;
+		}
+		
+	}
+	
+	void FixedUpdate () {
+		if (moveVector != Vector3.zero){
+			if(grounded == true){
+				rigidbody.AddForce( moveVector, ForceMode.VelocityChange);
+			}else{
+				rigidbody.AddForce( moveVector*.1f, ForceMode.VelocityChange);
+			}
+		}else{
+			rigidbody.AddForce( new Vector3(-rigidbody.velocity.x, 0f, -rigidbody.velocity.z), ForceMode.Acceleration);			
+		}
+		
+		if (rigidbody.velocity.y < 0){
+				rigidbody.AddForce( new Vector3(0, -rigidbody.velocity.y , 0) + Physics.gravity * fallSpeed, ForceMode.Acceleration);
+		}
+	}
+}
